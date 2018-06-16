1cc9f0d4bcdc01d3c05962c673bdcf060fe03e6c
MatrixInTheEast <40321261+MatrixInTheEast@users.noreply.github.com>
2018-06-16 11:56:25 -0400

    Add files via upload

---
+++ VGP123A1_TU_HOANG/Assets/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float speed;
+    public float lifetime;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if (lifetime <= 0)
+        {
+            lifetime = 2.0f;
+            Debug.LogWarning("Lifetime not set on " + name + ". Defaulting to " + lifetime);
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+
+
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+
+        Destroy(gameObject);
+    }
+
+
+}
+
+
+
+
+   
---
+++ VGP123A1_TU_HOANG/Assets/Script/Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;  
+
+
+public class Player : MonoBehaviour {
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+   
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    bool isAlive = true;
+
+    public Transform projectileSpawnPoint;
+    public Projectile projectilePrefab;
+    public float projectileSpeed;
+
+
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+
+        if (projectileSpeed <= 0)
+            {
+            projectileSpeed = 7.0f;
+            }
+        }
+
+    // Update is called once per frame
+    void Update () {
+        Run();
+        Jump();
+        FlipSprite();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    
+
+    }
+
+    void fire()
+    {
+        Projectile temp = Instantiate(projectilePrefab, projectileSpawnPoint.position,
+            projectileSpawnPoint.rotation);
+        temp.speed = projectileSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Collectible")
+        {
+            Destroy(c.gameObject);
+        }
+
+    }
+            private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHorizontalSpeed);
+        myAnimator.SetBool("Jump", playerHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidbody.velocity += jumpVelocityToAdd;  
+
+        }
+    }
+
+    
+}
